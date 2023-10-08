2574e6e5baa5e7f0d38ddad7a926233510da7137
bachirmeniai <122860647+bachirmeniai@users.noreply.github.com>
2023-10-08 16:24:26 +0100

    Update linkapi.dart

--- lib/linkapi.dart
+++ lib/linkapi.dart
@@ -1,5 +1,5 @@
 class AppLink {
-  static const String server = "http://fisaaaaaa.com/ecommerce";
+  static const String server = "http://fisaaaaaa.com/yemen";
   static const String imageststatic = "http://fisaaaaaa.com/ecommerce/upload";
 //========================== Image ============================
   static const String imagestCategories = "$imageststatic/categories";
